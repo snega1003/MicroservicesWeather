5c9f878997c9982f3cb6f0872731ce3b94eb36df
Snezhanna Roshchupkina <Snezhanna.Roshchupkina@lanit-tercom.com>
2022-05-23 19:23:33 +0300

    Add initial deps

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) => {
+  return Results.Ok(zip);
+});
 
 app.Run();

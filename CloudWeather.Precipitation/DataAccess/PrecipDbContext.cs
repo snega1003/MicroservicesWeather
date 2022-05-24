bf15744660748741328909cb9015a5d6fa3c22bd
Snezhanna Roshchupkina <Snezhanna.Roshchupkina@lanit-tercom.com>
2022-05-24 23:46:14 +0300

    add data access to precipitation service

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess
+{
+  public class PrecipDbContext : DbContext
+  {
+    private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Precipitation>(b => { b.ToTable("precipitation"); });
+    }
+
+    public PrecipDbContext() { }
+
+    public PrecipDbContext(DbContextOptions opts) : base(opts) { }
+
+    public DbSet<Precipitation> Precipitation { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      SnakeCaseIdentityTableNames(modelBuilder);
+    }
+  }
+}
